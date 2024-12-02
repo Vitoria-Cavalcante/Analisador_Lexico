aadf9f7e985f5123e52ec4e2c1c121c4698ba692
Vitória Cavalcante <lorencavalcante.23@gmail.com>
2024-12-02 19:02:22 -0300

    primeiro commit

---
+++ Lexer.cs
@@ -0,0 +1,82 @@
+﻿using Analisador_Lexico;
+
+namespace Analisador_Lexico.Core;
+
+public class Lexer {
+    private readonly InputReader _reader;
+    private readonly List<Token> _tokens = new();
+    private readonly ErrorHandler _errorHandler;
+
+    public Lexer(string sourceCode) {
+        _reader = new InputReader(sourceCode);
+        _errorHandler = new ErrorHandler();
+    }
+
+    public List<Token> Analyze() {
+        while (_reader.Current != '\0') {
+            SkipWhitespace();
+
+            if (char.IsLetter(_reader.Current)) {
+                ProcessIdentifier();
+            }
+            else if (char.IsDigit(_reader.Current)) {
+                ProcessNumber();
+            }
+            else {
+                ProcessSymbol();
+            }
+        }
+
+        return _tokens;
+    }
+
+    private void SkipWhitespace() {
+        while (char.IsWhiteSpace(_reader.Current)) {
+            _reader.Advance();
+        }
+    }
+
+    private void ProcessIdentifier() {
+        var startLine = _reader.Line;
+        var startColumn = _reader.Column;
+        var lexeme = "";
+
+        while (char.IsLetterOrDigit(_reader.Current)) {
+            lexeme += _reader.Current;
+            _reader.Advance();
+        }
+
+        _tokens.Add(new Token("IDENTIFIER", lexeme, startLine, startColumn));
+    }
+
+    private void ProcessNumber() {
+        var startLine = _reader.Line;
+        var startColumn = _reader.Column;
+        var lexeme = "";
+
+        while (char.IsDigit(_reader.Current)) {
+            lexeme += _reader.Current;
+            _reader.Advance();
+        }
+
+        _tokens.Add(new Token("NUMBER", lexeme, startLine, startColumn));
+    }
+
+    private void ProcessSymbol() {
+        var startLine = _reader.Line;
+        var startColumn = _reader.Column;
+        var lexeme = _reader.Current.ToString();
+
+        // Exemplo de tratamento de símbolos
+        if (lexeme == "+" || lexeme == "-" || lexeme == "=") {
+            _tokens.Add(new Token("SYMBOL", lexeme, startLine, startColumn));
+        }
+        else {
+            _errorHandler.AddError(startLine, startColumn, $"Símbolo inesperado: {lexeme}");
+        }
+
+        _reader.Advance();
+    }
+
+    public void PrintErrors() => _errorHandler.PrintErrors();
+}
---
+++ Main.cs
@@ -0,0 +1,24 @@
+﻿using Analisador_Lexico;
+using Analisador_Lexico.Core;
+
+namespace Analisador_Lexico;
+
+class Program {
+    static void Main(string[] args) {
+        string sourceCode = @"
+programa exemplo
+    variavel x;
+    x := 10;
+fimPrograma";
+
+        var lexer = new Lexer(sourceCode);
+        var tokens = lexer.Analyze();
+
+        Console.WriteLine("Tokens encontrados:");
+        foreach (var token in tokens) {
+            Console.WriteLine(token);
+        }
+
+        lexer.PrintErrors();
+    }
+}
